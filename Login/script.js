ca1d2d17f4dec44b4895a0c46a918d764fe09ab7
xGoUp <ahmadabdullatif987@gmail.com>
2023-06-20 20:21:06 +0700

    update

--- Login/script.js
+++ Login/script.js
@@ -1,23 +1,33 @@
-document.getElementById("login-form").addEventListener("submit", function(event) {
-    event.preventDefault(); // Prevent form submission
-  
-    // Retrieve the values entered by the user
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
-  
-    // Perform basic validation (you can add more advanced validation)
-    if (username === "" || password === "") {
-      alert("Please enter both username and password");
-      return;
-    }
-  
-    // Perform login logic (e.g., send an API request to a server for authentication)
-    // You can customize this part according to your specific requirements
-  
-    // Reset the form
-    document.getElementById("login-form").reset();
-  
-    // Redirect the user to the dashboard or another page
-    window.location.href = "dashboard.html";
-  });
-  
+// Mengambil elemen form pendaftaran dan masuk
+var registerForm = document.getElementById('register-form');
+var loginForm = document.getElementById('login-form');
+
+// Menangani submit form pendaftaran
+registerForm.addEventListener('submit', function(event) {
+  event.preventDefault(); // Mencegah form submit
+
+  // Mengambil nilai input pendaftaran
+  var registerUsername = document.getElementById('register-username').value;
+  var registerPassword = document.getElementById('register-password').value;
+
+  // Proses pendaftaran
+  // ...
+
+  // Tampilkan pesan sukses atau gagal
+  alert('Pendaftaran berhasil!');
+});
+
+// Menangani submit form masuk
+loginForm.addEventListener('submit', function(event) {
+  event.preventDefault(); // Mencegah form submit
+
+  // Mengambil nilai input masuk
+  var loginUsername = document.getElementById('login-username').value;
+  var loginPassword = document.getElementById('login-password').value;
+
+  // Proses masuk
+  // ...
+
+  // Tampilkan pesan sukses atau gagal
+  alert('Masuk berhasil!');
+});
