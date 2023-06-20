811c4f78607e98293fb8f6667e1d929ac79c4de9
xGoUp <ahmadabdullatif987@gmail.com>
2023-06-20 20:26:35 +0700

    update again

--- Login/script.js
+++ Login/script.js
@@ -1,11 +1,4 @@
-// Mengambil elemen form pendaftaran dan masuk
-var registerForm = document.getElementById('register-form');
-var loginForm = document.getElementById('login-form');
-
-// Menangani submit form pendaftaran
-registerForm.addEventListener('submit', function(event) {
-  event.preventDefault(); // Mencegah form submit
-
+function register() {
   // Mengambil nilai input pendaftaran
   var registerUsername = document.getElementById('register-username').value;
   var registerPassword = document.getElementById('register-password').value;
@@ -13,14 +6,11 @@ registerForm.addEventListener('submit', function(event) {
   // Proses pendaftaran
   // ...
 
-  // Tampilkan pesan sukses atau gagal
-  alert('Pendaftaran berhasil!');
-});
+  // Tampilkan pesan sukses atau gagal dengan animasi
+  showMessage('Pendaftaran berhasil!');
+}
 
-// Menangani submit form masuk
-loginForm.addEventListener('submit', function(event) {
-  event.preventDefault(); // Mencegah form submit
-
+function login() {
   // Mengambil nilai input masuk
   var loginUsername = document.getElementById('login-username').value;
   var loginPassword = document.getElementById('login-password').value;
@@ -28,6 +18,20 @@ loginForm.addEventListener('submit', function(event) {
   // Proses masuk
   // ...
 
-  // Tampilkan pesan sukses atau gagal
-  alert('Masuk berhasil!');
-});
+  // Tampilkan pesan sukses atau gagal dengan animasi
+  showMessage('Masuk berhasil!');
+}
+
+function showMessage(message) {
+  var messageElement = document.createElement('div');
+  messageElement.className = 'message';
+  messageElement.textContent = message;
+  document.body.appendChild(messageElement);
+
+  setTimeout(function() {
+    messageElement.classList.add('fade-out');
+    setTimeout(function() {
+      messageElement.remove();
+    }, 1000);
+  }, 2000);
+}
